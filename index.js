df710cf0957545a33b7262e8607cfed1eb7ef725
Rishikesh Tiwari <rishi@kanris.biz>
2018-03-30 15:32:18 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+/* const path = require('path'); */
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+var cors = require('cors');
+var url = require('url');
+const app = express();
+
+
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+// app.use('/public', express.static(path.join(__dirname, 'public'))); 
+//app.use(cors());
+app.use(cors({
+    origin: 'http://localhost'
+}));
+
+app.get('/html2pdf', (req, res) => {
+    /* createUrl = req.body.link; */
+    var url_parts = url.parse(req.url, true);
+    var query = url_parts.query;
+    var link = req.query.link;
+    var pdf_name;
+    if (!req.query.name) {
+        pdf_name = 'test.pdf'
+    } else {
+        pdf_name = req.query.name + '.pdf';
+    }
+    var pdf = function(args) {
+            return args;
+        }
+        (async() => {
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+            await page.goto(link, { waitUntil: 'networkidle2' });
+            await page.emulateMedia('screen');
+            await page.waitFor(30000);
+            var pdf = await page.pdf({ format: 'Letter', printBackground: true, });
+            await browser.close();
+            console.log("pdf converted")
+            res.setHeader('Content-Type', 'application/pdf');
+            res.setHeader('Content-Disposition', 'attachment; filename=' + pdf_name);
+            //var data = fs.readFileSync('./test.pdf');
+            res.send(pdf);
+        })();
+});
+
+app.listen(3000, () => console.log('server started.....'));
